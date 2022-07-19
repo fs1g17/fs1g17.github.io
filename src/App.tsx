5c53516f6810bf1ade72b4a2551f51be9c4f6fff
fs1g17 <47851444+fs1g17@users.noreply.github.com>
2022-07-19 21:17:32 +0100

    Basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { BrowserRouter } from "react-router-dom";
+
+import { Navbar } from './components';
+import About from './screens/About/About';
+import Blog from './screens/Blog/Blog';
+import Contact from './screens/Contact/Contact';
+import Newsletter from './screens/Newsletter/Newsletter';
+import Shop from './screens/Shop/Shop';
 
 const theme = createTheme({
   palette: {
@@ -20,16 +28,19 @@ const theme = createTheme({
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <header className="App-header">
-            <p>Hello</p>
-          </header>
-        </div>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newsletter" element={<Newsletter />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/screens/Blog/Blog.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const Blog: FC = () => {
+
+    return(
+        <div>
+            Blog
+        </div>
+    );
+};
+
+export default Blog;
