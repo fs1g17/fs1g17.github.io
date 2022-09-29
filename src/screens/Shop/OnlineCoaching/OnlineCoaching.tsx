33e0182c87ae8a96b39a7a484ac207d3b79081a6
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-09-29 19:58:25 +0100

    Add image to the online-coaching section

--- src/screens/Shop/OnlineCoaching/OnlineCoaching.tsx
+++ src/screens/Shop/OnlineCoaching/OnlineCoaching.tsx
@@ -1,11 +1,14 @@
-import { Typography } from '@mui/material';
 import React from 'react';
+
+import { Box, Typography } from '@mui/material';
+
+import img from '../../../static/images/shop-card-img-1.png';
 
 const OnlineCoaching = () => {
   return (
-    <div style={{ marginTop: 400}}>
-      <Typography variant='h1'>THIS IS COACHING PAGE</Typography>
-    </div>  
+    <Box mt="64px" height="calc(100vh - 62px);">
+      <img src={img} style={{ height: '100%'}} />
+    </Box>
   );
 }
 
