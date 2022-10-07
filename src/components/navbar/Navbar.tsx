6602511ad178974d3a2a394d90e65b10e078e405
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-10-07 18:55:18 +0100

    Make the toolbar mobile friendly

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,8 +8,16 @@ import {
   Typography,
   Toolbar,
   useTheme,
-  Box
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  IconButton,
+  Drawer
 } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 import NavbarLink from "./NavbarLink";
 
 const StyledAppBar = styled(AppBar)<AppBarProps>(({ theme }) => ({
@@ -26,6 +34,7 @@ interface NavItem {
   label: string;
 }
 
+const drawerWidth = 240;
 const navItems: NavItem[] = [
   {
     path: "/about",
@@ -48,6 +57,29 @@ const navItems: NavItem[] = [
 const Navbar: FC = () => {
   const theme = useTheme();
   const location = useLocation();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map(({ path, label }) => (
+          <ListItem key={path} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }} href={path}>
+              <ListItemText primary={label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   const getColor = (targetLocation: string) => {
     return location.pathname.toString().includes(targetLocation)
@@ -55,17 +87,48 @@ const Navbar: FC = () => {
       : "#FFFFFF";
   };
 
+  const container = window !== undefined ? () => window.document.body : undefined;
+
   return (
-    <StyledAppBar>
-      <Toolbar>
-        <Typography variant="h4">Navbar</Typography>
-        <Box sx={{ marginLeft: "auto" }}>
-          {navItems.map(({ path, label }) => (
-            <NavbarLink path={path} label={label} color={getColor(path)} />
-          ))}
-        </Box>
-      </Toolbar>
-    </StyledAppBar>
+    <>
+      <StyledAppBar>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Navbar
+          </Typography>
+          <Box sx={{ marginLeft: "auto", display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map(({ path, label }) => (
+              <NavbarLink path={path} label={label} color={getColor(path)} />
+            ))}
+          </Box>
+        </Toolbar>
+      </StyledAppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{ display: { xs: 'block', sm: 'none' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }}}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </>
   );
 };
 
